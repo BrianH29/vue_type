e653098ebad189a7166fd208dc30e8099bbfca27
BrianH <brianhwang29@gmail.com>
2021-12-30 11:25:50 +0900

    input component setup

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>Vue Todo with TypeScript</h1>
+    <!--    다른 방법으로 작성하기 - notion 정리 -->
+    <!--    <TodoInput v-model="todoText" @add="addTodoItem"></TodoInput>-->
+    <TodoInput
+      :item="todoText"
+      @input="updateTodoText"
+      @add="addTodoItem"
+    ></TodoInput>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from 'vue';
+import TodoInput from '@/components/TodoInput.vue';
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: '',
+    };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = '';
+    },
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
